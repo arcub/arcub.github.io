04315e485aaea94c90518475a59038ae548284b2
tomos <tf10@outlook.com>
2017-10-26 00:20:24 +0100

    added initial files

---
+++ php-containers/navigation.php
@@ -0,0 +1,70 @@
+<?php
+echo "
+<nav class='navbar navbar-default'>
+  <div class='container-fluid'> 
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class='navbar-header'>
+      <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#defaultNavbar1'><span class='sr-only'>Toggle navigation</span><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button>
+      <a class='navbar-brand' href='#'>Aberyswyth Robotics Club</a></div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    
+    <div class='collapse navbar-collapse' id='defaultNavbar1'>
+      <ul class='nav navbar-nav'>
+        <li><a href=''>Home<span class='sr-only'>(current)</span></a></li>
+        <li><a href='projects/' target='new'>Projects</a></li>
+        <li><a href='http://github.com/arcub/committee/' target='new'>Committee</a></li>
+        <li class='dropdown'><a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-expanded='false'>Projects 2017<span class='caret'></span></a>
+          <ul class='dropdown-menu' role='menu'>
+            <li><a href='https://github.com/arcub/magician-chassis/' target='new'>Magician Chassis</a></li>
+            <li class='divider'></li>
+            <li><a href='https://github.com/arcub/sumo2017/' target='new'>Sumo Robots</a></li>
+            <li><a href='https://github.com/arcub/pioneer2017/' target='new'>Pioneer</a></li>
+            <li><a href='https://github.com/arcub/openrov2017/' target='new'>OpenROV</a></li>
+            <li><a href='https://github.com/arcub/rocky2017/' target='new'>Rocky</a></li>
+            <li><a href='https://github.com/arcub/humanoid2017/' target='new'>Humanoid</a></li>
+            <li><a href='https://github.com/arcub/boat2017/' target='new'>Boat</a></li>
+          </ul>
+        </li>
+      </ul>
+      <img class='nav navbar-nav navbar-right' src='img/aulogo.png' width='20%' alt='Aberystwyth University Logo' />
+      <!--<form class='navbar-form navbar-left' role='search'>
+        <div class='form-group'>
+          <input type='text' class='form-control' placeholder='Search'>
+        </div>
+        <button type='submit' class='btn btn-default'>Submit</button>
+      </form>-->
+      <!--<ul class='nav navbar-nav navbar-right'>
+        <li><a href='#'>Link</a></li>
+        <li class='dropdown'><a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-expanded='false'>Dropdown<span class='caret'></span></a>
+          <ul class='dropdown-menu' role='menu'>
+            <li><a href='#'>Action</a></li>
+            <li><a href='#'>Another action</a></li>
+            <li><a href='#'>Something else here</a></li>
+            <li class='divider'></li>
+            <li><a href='#'>Separated link</a></li>
+          </ul>
+        </li>-->
+      </ul>
+    </div>
+    <!-- /.navbar-collapse --> 
+  </div>
+  <!-- /.container-fluid --> 
+</nav>
+
+<div class='container-fluid'>
+  <div class='row'>
+    <div class='col-md-6 col-md-offset-3'>
+	  <h1 class='text-center'><img src='img/logo.png' width='20%' class='logo'/></h1>
+      <h1 class='text-center'>Aberystwyth Robotics Club<br />University Branch</h1>
+    </div>
+  </div>
+</div>
+
+<div class='container-fluid'>
+  <div class='row'>
+    <div>
+      <h1 class='text-center'><img src='img/header.jpg' width='5472' class='img-responsive'/></h1>
+    </div>
+  </div>
+</div>"
+?>
