0f37351dd8091f18611bd0278d413117935d9699
Tomos Fearn <tfearn10@outlook.com>
2018-09-11 01:42:45 +0100

    added 2018-2019 website

---
+++ nav.js
@@ -0,0 +1,42 @@
+var bindNavigation = "";
+
+
+bindNavigation +=  "<div class='container-fluid'>";
+bindNavigation +=    "<div class='navbar-header'>";
+bindNavigation +=      "<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#myNavbar'>";
+bindNavigation +=        "<span class='icon-bar'></span>";
+bindNavigation +=        "<span class='icon-bar'></span>";
+bindNavigation +=        "<span class='icon-bar'></span>";
+bindNavigation +=      "</button>";
+bindNavigation +=      "<a class='navbar-brand' href='http://students.aberrobotics.club/'>Aber Robotics Club</a>";
+bindNavigation +=    "</div>";
+bindNavigation +=    "<div class='collapse navbar-collapse' id='myNavbar'>";
+bindNavigation +=      "<ul class='nav navbar-nav navbar-right'>";
+bindNavigation +=        "<li><a href='http://students.aberrobotics.club/'>HOME</a></li>";
+bindNavigation +=        "<li><a href='http://students.aberrobotics.club/projects'>PROJECTS</a></li>";
+bindNavigation +=        "<li class='dropdown'>";
+bindNavigation +=          "<a class='dropdown-toggle' data-toggle='dropdown' href='#'>EVENTS";
+bindNavigation +=         "<span class='caret'></span></a>";
+bindNavigation +=          "<ul class='dropdown-menu'>";
+bindNavigation +=            "<li><a href='http://students.aberrobotics.club/freshers'>FRESHERS FAIR</a></li>";
+bindNavigation +=			 "<li><div class='divider'></li>";
+bindNavigation +=            "<li><a href='http://students.aberrobotics.club/welcome'>WELCOME WORKSHOP</a></li>";
+bindNavigation +=          "</ul>";
+bindNavigation +=        "</li>";
+bindNavigation +=        "<li class='dropdown'>";
+bindNavigation +=          "<a class='dropdown-toggle' data-toggle='dropdown' href='#'>PROJECTS 2018";
+bindNavigation +=          "<span class='caret'></span></a>";
+bindNavigation +=          "<ul class='dropdown-menu'>";
+bindNavigation +=            "<li><a href='http://students.aberrobotics.club/github'>VISIT GITHUB</a></li>";
+bindNavigation +=          "</ul>";
+bindNavigation +=        "</li>";
+bindNavigation +=        "<li><a href='http://students.aberrobotics.club/shop'>SHOP</a></li>";
+bindNavigation +=        "<li><a href='http://students.aberrobotics.club/#contact'>CONTACT</a></li>";
+bindNavigation +=        "<!--<li><a href='#'><span class='glyphicon glyphicon-search'></span></a></li>-->";
+bindNavigation +=      "</ul>";
+bindNavigation +=    "</div>";
+bindNavigation +=  "</div>";
+
+function navjs() {
+	document.getElementById('navjs').innerHTML = bindNavigation;
+}
