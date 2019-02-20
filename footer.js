739c84a43a724645129f72c2ef51877c47bb23ed
Tomos Fearn <tfearn10@outlook.com>
2019-02-20 14:57:06 +0000

    added footer js

---
+++ footer.js
@@ -0,0 +1,3 @@
+function footerjs() {
+	document.getElementById('footer').innerHTML = "Copyright &copy; 2019 &middot; All Rights Reserved &middot; <a href='http://students.aberrobotics.club/' >Aberystwyth Robotics Club</a>";
+}
