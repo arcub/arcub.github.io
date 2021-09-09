693dcf62e0672d5198eeab63aab6d99328c1f700
Tomos <tomos@atlas.local>
2021-09-09 16:56:13 +0000

    updated site ssl link

--- footer.js
+++ footer.js
@@ -1,3 +1,3 @@
 function footerjs() {
-	document.getElementById('footer').innerHTML = "Copyright &copy; 2020 &middot; All Rights Reserved &middot; <a href='http://students.aberrobotics.club/' >Aberystwyth Robotics Club</a>";
+	document.getElementById('footer').innerHTML = "Copyright &copy; 2020 &middot; All Rights Reserved &middot; <a href='https://students.aberrobotics.club/' >Aberystwyth Robotics Club</a>";
 }
